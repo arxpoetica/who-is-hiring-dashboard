3a681d059bb0e25d65dc26b7bf41219f54f8d430
Robert Hall <thebookofrobert@gmail.com>
2019-06-10 13:25:00 -0600

    Semi-optimize search / filters

--- src/routes/listing/[id].svelte
+++ src/routes/listing/[id].svelte
@@ -31,63 +31,59 @@
 	import Card from '../_components/Card.svelte'
 
 	let listing
+	let posts = []
 
-	$: tags = $filterSet.map(set => set.value)
-	$: languagesTags = $languageSet.map(set => set.value)
-
-	$: posts = listing ? listing.children.map(post => {
-		// just indexing and optimizing for faster search / filter
-		post.text = typeof post.text === 'string' ? post.text.replace(/\<a /gi, '<a target="_blank" ') : ''
-		const doc = new DOMParser().parseFromString(post.text, 'text/html')
-		post.searchText = doc.body.textContent.replace(/\s\s+/g, ' ').toLowerCase()
-		post.tags = tags.filter(tag => post.searchText.indexOf(tag) > -1)
-		post.languages = languagesTags.filter(tag => post.searchText.indexOf(tag) > -1)
-		post.hide = $hide.indexOf(post.id) > -1
-		post.apply = $apply.indexOf(post.id) > -1
-		post.applied = $applied.indexOf(post.id) > -1
-		return post
-	}) : []
-
-	$: queryPosts = $query.length > 2
-		? posts.filter(post => post.searchText.indexOf($query) > -1)
-		: posts
-
+	$: hasQuery = $query.length > 2
 	$: showHidden = $settings.find(setting => setting.value === 'hidden').on
 	$: showHiddenOnly = $settings.find(setting => setting.value === 'hiddenonly').on
 	$: showToApply = $settings.find(setting => setting.value === 'apply').on
 	$: showAppliedTo = $settings.find(setting => setting.value === 'applied').on
-	$: settingsPosts = queryPosts.filter(post => {
+	$: languages = $languageSet.filter(set => set.on).map(set => set.value)
+	$: filters = $filterSet.filter(set => set.on).map(set => set.value)
+	$: filteredPosts = posts.filter(post => {
+		let show = true
+		if (hasQuery) {
+			show = show && post.searchText.indexOf($query) > -1
+		}
 		if (showHiddenOnly) {
-			return post.hide
+			show = show && post.hide
 		} else {
-			let show = showHidden ? true : !post.hide
+			show = show && (showHidden ? true : !post.hide)
 			show = show && (showToApply ? post.apply : true)
 			show = show && (showAppliedTo ? post.applied : true)
-			return show
 		}
-	})
-
-	$: languages = $languageSet.filter(set => set.on).map(set => set.value)
-	$: languagesPosts = languages.length
-		? settingsPosts.filter(post => {
+		if (languages.length) {
 			const found = languages.filter(language => post.languages.indexOf(language) > -1)
-			return found.length === languages.length
-		})
-		: settingsPosts
-
-	$: filters = $filterSet.filter(set => set.on).map(set => set.value)
-	$: filteredPosts = filters.length
-		? languagesPosts.filter(post => {
+			show = show && (found.length === languages.length)
+		}
+		if (filters.length) {
 			const found = filters.filter(filter => post.tags.indexOf(filter) > -1)
-			return found.length === filters.length
-		})
-		: languagesPosts
+			show = show && (found.length === filters.length)
+		}
+		return show
+	})
 
 	const { page } = stores()
 	onMount(async () => {
 		const res = await fetchHN(`items/${$page.params.id}`)
 		listing = await res.json()
-		// console.log(listing)
+		console.log(listing)
+
+		const tags = $filterSet.map(set => set.value)
+		const languagesTags = $languageSet.map(set => set.value)
+
+		posts = listing.children.map(post => {
+			// just indexing and optimizing for faster search / filter
+			post.text = typeof post.text === 'string' ? post.text.replace(/\<a /gi, '<a target="_blank" ') : ''
+			const doc = new DOMParser().parseFromString(post.text, 'text/html')
+			post.searchText = doc.body.textContent.replace(/\s\s+/g, ' ').toLowerCase()
+			post.tags = tags.filter(tag => post.searchText.indexOf(tag) > -1)
+			post.languages = languagesTags.filter(tag => post.searchText.indexOf(tag) > -1)
+			post.hide = $hide.indexOf(post.id) > -1
+			post.apply = $apply.indexOf(post.id) > -1
+			post.applied = $applied.indexOf(post.id) > -1
+			return post
+		})
 	})
 </script>
 
